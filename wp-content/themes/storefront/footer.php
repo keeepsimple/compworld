a5280df4bf62b4e55dd333f2f4dc8ea45c8301ed
Quang <quangbeo199x@gmail.com>
2021-11-03 18:05:53 +0700

    upload website

---
+++ wp-content/themes/storefront/footer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after
+ *
+ * @package storefront
+ */
+
+?>
+
+		</div><!-- .col-full -->
+	</div><!-- #content -->
+
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="col-full">
+
+			© 2021 Công ty Cổ phần đầu tư công nghệ CompWorld<br>
+
+			Địa chỉ: Tây Hồ, Hà Nội<br>
+
+			GPĐKKD số 0101161194 do Sở KHĐT Tp.Hà Nội cấp ngày 31/8/2001<br>
+
+			Email: admin@compworld.com. Điện thoại: 1900 1909
+
+		</div><!-- .col-full -->
+	</footer><!-- #colophon -->
+
+
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
